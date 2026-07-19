68c24a05c42e83d5fe05ce0bfbcf0cce3f87a07f
TheSriramM <161264109+TheSriramM@users.noreply.github.com>
2026-07-19 22:27:20 +1200

    js for the notifications

--- static/js/dashboard_base.js
+++ static/js/dashboard_base.js
@@ -1,12 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Every element with class toast message and storing them in a list (NodeList)
     const toasts = document.querySelectorAll(".toast-message");
 
+    // Looping through each notification
     toasts.forEach((toast) => {
+        
+        // 3 second timer
+        // The code inside the block will trigger once the 3 seconds are over
         setTimeout(() => {
+            // These two lines work together to decrease the opacity of the toast notification to zero over 0.4 seconds
             toast.style.transition = "opacity 0.4s ease";
             toast.style.opacity = "0";
 
+            // Once the 0.4 seconds are over, the toast notification is deleted
             setTimeout(() => toast.remove(), 400);
         }, 3000);
+
     });
+
 });
