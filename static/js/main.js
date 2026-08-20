6200a4d6bdcad0ae76c9ebc1ac9df2e86aed73b0
TheSriramM <161264109+TheSriramM@users.noreply.github.com>
2026-08-20 18:42:18 +1200

    username and password length counters in the register page working

--- static/js/main.js
+++ static/js/main.js
@@ -4,22 +4,30 @@ document.addEventListener("DOMContentLoaded", () => {
     const usernameInput = document.getElementById("username");
     const usernameCount = document.getElementById("username-count");
 
+    // If the elements in the const variables above exist
     if (usernameInput && usernameCount) {
         usernameInput.addEventListener("input", () => {
 
+            // Get the length of the current username
             const length = usernameInput.value.length;
+
+            // Display the length of the current username
             usernameCount.textContent = `${length} / 30`;
 
         });
     }
 
+    // If the elements in the const variables above exist
     const passwordInput = document.getElementById("password");
     const passwordCount = document.getElementById("password-count");
 
     if (passwordInput && passwordCount) {
         passwordInput.addEventListener("input", () => {
 
+            // Get the length of the current password
             const length = passwordInput.value.length;
+
+            // Display the length of the current password
             passwordCount.textContent = `${length} / 64`;
 
         });
